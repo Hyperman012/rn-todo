56d11bcc4c52bed3cbaa7e1355e35f5776f2140f
Adam Whaley <adam@industriallogic.com>
2024-02-21 14:12:20 -0600

    style card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
 import { Todo } from './Todo.tsx';
+import React from 'react';
 
 export function Card(props: { todo: Todo }) {
     return (
-        <View>
-            <Text>{props.todo.title}</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <ImageBackground
+                style={{ height: 100, width: 100, alignItems: 'center', justifyContent: 'center' }}
+                source={require('./assets/todo.png')}>
+                <Text>{props.todo.title}</Text>
+            </ImageBackground>
         </View>
     );
 }
