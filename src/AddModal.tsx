da49ace727b3f6d14663abbef3ffa95def39843e
Adam Whaley <adam@industriallogic.com>
2024-02-21 15:25:36 -0600

    add close button to modal

--- src/AddModal.tsx
+++ src/AddModal.tsx
@@ -1,10 +1,14 @@
-import { Modal, TextInput } from 'react-native';
+import { Modal, Pressable, Text, TextInput } from 'react-native';
 import React, { useState } from 'react';
 
 export function AddModal() {
     const [newTodo, setNewTodo] = useState('');
+    const [isVisible, setIsVisible] = useState(true);
     return (
-        <Modal>
+        <Modal visible={isVisible}>
+            <Pressable onPress={() => setIsVisible(false)}>
+                <Text>Close</Text>
+            </Pressable>
             <TextInput placeholder={'Enter To-Do'} onChangeText={setNewTodo} value={newTodo} />
         </Modal>
     );
--- src/__tests__/AddModal.test.tsx
+++ src/__tests__/AddModal.test.tsx
@@ -1,20 +1,30 @@
 import 'react-native';
-import { describe, it } from '@jest/globals';
-import { fireEvent, render } from '@testing-library/react-native';
+import { beforeEach, describe, expect, it } from '@jest/globals';
+import { fireEvent, render, RenderResult } from '@testing-library/react-native';
 import React from 'react';
 import { AddModal } from '../AddModal.tsx';
 
 describe('AddModal', () => {
+    let modal: RenderResult;
+    beforeEach(() => {
+        modal = render(<AddModal />);
+    });
+
     it('has text input', () => {
-        const modal = render(<AddModal />);
         modal.getByPlaceholderText('Enter To-Do');
     });
 
     it('sets text value', () => {
-        const modal = render(<AddModal />);
         const input = modal.getByPlaceholderText('Enter To-Do');
         fireEvent.changeText(input, 'do the dishes');
-
         modal.getByDisplayValue('do the dishes');
+    });
+
+    it('closes', () => {
+        fireEvent.press(modal.getByText('Close'));
+
+        expect(() => {
+            modal.getByPlaceholderText('Enter To-Do');
+        }).toThrow();
     });
 });
