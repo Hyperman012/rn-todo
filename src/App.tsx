d6e52914fd763933d9a4d9d5c0c5ab6651f49296
Adam Whaley <adam@industriallogic.com>
2024-02-21 13:51:59 -0600

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { SafeAreaView, useColorScheme } from "react-native";
+import React from 'react';
+import { SafeAreaView, useColorScheme } from 'react-native';
 
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import { createMediaStyleSheet } from "media-style-sheet";
-import { Card } from "./Card.tsx";
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { createMediaStyleSheet } from 'media-style-sheet';
+import { Card } from './Card.tsx';
 
 const MediaStyleSheet = createMediaStyleSheet({
-  tablet: () => true,
-  mobile: () => false,
+    tablet: () => true,
+    mobile: () => false,
 });
 
 function useIsDarkMode() {
-  return useColorScheme() === "dark";
+    return useColorScheme() === 'dark';
 }
 
 export default function App(): React.JSX.Element {
-  const isDarkMode = useIsDarkMode();
+    const isDarkMode = useIsDarkMode();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <Card />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <Card todo={{ title: 'Make List' }} />
+        </SafeAreaView>
+    );
 }
