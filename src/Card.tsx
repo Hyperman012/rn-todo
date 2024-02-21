38228f8465947ee97b0e0f8989ca96be5d89e576
Kody Fintak <kody.fintak@gmail.com>
2024-02-21 17:29:22 -0500

    extract smaller components out of Card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,26 +1,43 @@
-import { ImageBackground, Text, TextInput, View } from 'react-native';
+import { ImageBackground, Pressable, Text, TextInput, View } from 'react-native';
 import { Todo } from './Todo.tsx';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 export function Card(props: { todo: Todo }) {
     return (
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <ImageBackground
-                style={{ height: 100, width: 100, alignItems: 'center', justifyContent: 'center' }}
-                source={require('./assets/todo.png')}>
+        <TodoPostIt>
+            <View style={{ flex: 1, paddingVertical: 10, gap: 10 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <DeleteTodoButton />
+                </View>
                 <Text>{props.todo.title}</Text>
-            </ImageBackground>
-        </View>
+            </View>
+        </TodoPostIt>
+    );
+}
+
+function DeleteTodoButton() {
+    return (
+        <Pressable>
+            <Text style={{ fontWeight: 'bold' }}>x</Text>
+        </Pressable>
     );
 }
 
 export function EditableCard(props: { onChangeText: (text: string) => void; text: string }) {
     return (
+        <TodoPostIt>
+            <TextInput placeholder={'Enter To-Do'} onChangeText={props.onChangeText} value={props.text} />
+        </TodoPostIt>
+    );
+}
+
+function TodoPostIt(props: PropsWithChildren) {
+    return (
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
             <ImageBackground
                 style={{ height: 100, width: 100, alignItems: 'center', justifyContent: 'center' }}
                 source={require('./assets/todo.png')}>
-                <TextInput placeholder={'Enter To-Do'} onChangeText={props.onChangeText} value={props.text} />
+                {props.children}
             </ImageBackground>
         </View>
     );
