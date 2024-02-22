37a31366f599fd9cfb54954a4b7430706140f1b1
Kody Fintak <kody.fintak@gmail.com>
2024-02-21 22:15:38 -0500

    make TodoCards FlatList

--- src/todoCard/TodoCards.tsx
+++ src/todoCard/TodoCards.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { FlatList } from 'react-native';
 import { TodoCard } from './TodoCard.tsx';
 import React from 'react';
 import { useTodos } from '../TodoProvider.tsx';
@@ -6,10 +6,6 @@ import { useTodos } from '../TodoProvider.tsx';
 export function TodoCards() {
     const todo = useTodos();
     return (
-        <View>
-            {todo.todos.map((todo, index) => (
-                <TodoCard key={index} todo={todo} />
-            ))}
-        </View>
+        <FlatList data={todo.todos} renderItem={listItem => <TodoCard key={listItem.index} todo={listItem.item} />} />
     );
 }
