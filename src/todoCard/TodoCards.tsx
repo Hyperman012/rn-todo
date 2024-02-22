965a89d080680c7f1accd05e0423de65a3bef3b8
Adam Whaley <adam@industriallogic.com>
2024-02-22 08:29:36 -0600

    add reading todo from context

--- src/todoCard/TodoCards.tsx
+++ src/todoCard/TodoCards.tsx
@@ -1,11 +1,15 @@
 import { View } from 'react-native';
 import { TodoCard } from './TodoCard.tsx';
 import React from 'react';
+import { useTodos } from '../TodoProvider.tsx';
 
 export function TodoCards() {
+    const todos = useTodos();
     return (
         <View>
-            <TodoCard todo={{ title: 'Make List' }} />
+            {todos.map(item => (
+                <TodoCard todo={item} />
+            ))}
         </View>
     );
 }
